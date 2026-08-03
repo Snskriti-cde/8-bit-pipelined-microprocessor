416abffdfdb6537620546f61632e6c78a44ef2a0
Sanskriti Panchal <sanskritipanchal108@gmail.com>
2026-08-03 19:43:55 +0530

    Update alu_control.v

--- RTL/CONTROL/alu_control.v
+++ RTL/CONTROL/alu_control.v
@@ -1,9 +1,9 @@
 `include "alu_defs.vh"
 
 module alu_control (
-    input      [2:0] alu_op,   
-    input      [5:0] funct,     
-    output reg [4:0] alu_control  
+    input      [2:0] alu_op,    
+    input      [5:0] funct,    
+    output reg [4:0] alu_control
 );
 
     // alu_op classes (must match control_unit.v)
@@ -14,7 +14,7 @@ module alu_control (
     localparam F_ADD=6'h20, F_SUB=6'h22, F_AND=6'h24, F_OR=6'h25, F_XOR=6'h26,
                F_NOR=6'h27, F_SLT=6'h2A, F_SLL=6'h00, F_SRL=6'h02, F_SRA=6'h03,
                F_ROL=6'h10, F_ROR=6'h11, F_JR=6'h08, F_NOT=6'h21, F_PASA=6'h23,
-               F_INC=6'h12, F_DEC=6'h13m F_MUL=6'h18, F_DIV=6'h1A;
+               F_INC=6'h12, F_DEC=6'h13, F_MUL=6'h18, F_DIV=6'h1A;
 
     always @(*) begin
    // default 
@@ -44,8 +44,8 @@ module alu_control (
                     F_PASA: alu_control = `ALU_PASA;    
                     F_INC:  alu_control = `ALU_INC;     
                     F_DEC:  alu_control = `ALU_DEC;
-	            F_MUL:  alu_control = `ALU_MUL;
-		    F_DIV:  alu_control = `ALU_DIV;
+	                F_MUL:  alu_control = `ALU_MUL;
+		            F_DIV:  alu_control = `ALU_DIV;
                     default: alu_control = `ALU_ADD; // JR/unknown: ALU result unused
                 endcase
             end
