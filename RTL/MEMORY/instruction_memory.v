54c506aa25bb365d4abfb1af1724c018b9188c2e
Sanskriti Panchal <sanskritipanchal108@gmail.com>
2026-08-03 21:46:09 +0530

    Update instruction_memory.v

--- RTL/MEMORY/instruction_memory.v
+++ RTL/MEMORY/instruction_memory.v
@@ -1,14 +1,22 @@
 `timescale 1ns / 1ps
-module Instruction_Memory(
-input[7:0] Address, //address from program counter
-output[31:0] Instr //instruction word (32-bit)
-    );
-   
- reg[31:0] Instr_Array[0:255]; 
- initial
-   begin
-   Instr_Array[0]=32'h9c367a34;
-     Instr_Array[1] = 32'h000458bc;
-   end
-   assign Instr = Instr_Array[Address];
+
+module Instruction_Memory #(
+    parameter MEM_FILE = "program.hex"
+)(
+    input      [7:0]  Address,   // word-addressed, from the program counter
+    output     [31:0] Instr
+);
+
+    reg [31:0] Instr_Array [0:255];
+    integer i;
+
+    initial begin
+        for (i = 0; i < 256; i = i + 1)
+            Instr_Array[i] = 32'h0000_0000;
+        
+            $readmemh(MEM_FILE, Instr_Array);
+    end
+
+    assign Instr = Instr_Array[Address];
+
 endmodule
